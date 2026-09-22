e35b5c001cb0e4dbdaf4995ba1a6844fb0249836
Isaac Silva <isaacsilta.info@gmail.com>
2026-09-22 10:30:31 -0300

    primeiro commit

---
+++ src/main/java/br/com/isaacsilva/tech/oauth2/SpringBootOauth2RestApplication.java
@@ -0,0 +1,13 @@
+package br.com.isaacsilva.tech.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2RestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/isaacsilva/tech/oauth2/SpringBootOauth2RestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isaacsilva.tech.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
